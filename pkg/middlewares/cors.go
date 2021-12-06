0d2fe89ab51547528c974cc8591002bcb81a3d5d
tangx <uyinn@live.com>
2021-12-06 13:09:42 +0800

    fix: 增加跨域 middleware 快捷方法

---
+++ pkg/middlewares/cors.go
@@ -0,0 +1,37 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tangx/rum-gonic/rum"
+)
+
+// DefaultCorsPolicy 默认跨域规则， 所有来源
+func DefaultCorsPolicy() rum.MiddlewareOperator {
+	return CorsPolicy("*")
+}
+
+// CorsPolicy 允许跨域来源
+// example, sorigin = https://developer.mozilla.org
+func CorsPolicy(origin string) rum.MiddlewareOperator {
+
+	cors := func(c *gin.Context) {
+		method := c.Request.Method
+		if method != "" {
+			c.Header("Access-Control-Allow-Origin", origin) // 可将将 * 替换为指定的域名
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization,X-Token")
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
+			c.Header("Access-Control-Allow-Credentials", "true")
+		}
+
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+
+		c.Next()
+	}
+
+	return rum.NewMiddleware(cors)
+}
