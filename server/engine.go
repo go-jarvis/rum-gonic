49fc656ba835e8ec4a35e4895146e0484e5a6246
tangx <uyinn28@gmail.com>
2022-12-06 22:16:17 +0800

    fix: rename srver

--- server/engine.go
+++ server/engine.go
@@ -7,36 +7,36 @@ import (
 	"github.com/tangx/ginbinder"
 )
 
-type Server struct {
+type rumServer struct {
 	engine *gin.Engine
 }
 
-func Default() *Server {
+func Default() *rumServer {
 	e := gin.Default()
 
-	return &Server{
+	return &rumServer{
 		engine: e,
 	}
 }
 
-func (e *Server) Run(addr string) error {
+func (e *rumServer) Run(addr string) error {
 	return e.engine.Run(addr)
 }
 
 type HandlerFunc = gin.HandlerFunc
 
-func (e *Server) Use(fns ...HandlerFunc) {
+func (e *rumServer) Use(fns ...HandlerFunc) {
 	for _, fn := range fns {
 		e.engine.Use(fn)
 	}
 }
 
-func (e *Server) Handle(handlers ...HanlderOperator) {
+func (e *rumServer) Handle(handlers ...HanlderOperator) {
 	for _, h := range handlers {
-		e.handle(h)
+		_, _ = e.handle(h)
 	}
 }
-func (e *Server) handle(handler HanlderOperator) (interface{}, error) {
+func (e *rumServer) handle(handler HanlderOperator) (interface{}, error) {
 
 	e.engine.Handle(handler.Methods(), handler.Path(), handle(handler))
 	return nil, nil
