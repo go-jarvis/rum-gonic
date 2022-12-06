fbf5136c0d18c4e377a6f32f5f0b2eb754e7ed7d
tangx <uyinn28@gmail.com>
2022-12-06 16:46:48 +0800

    feat: add span logger

--- pkg/middlewares/trace/trace_logr.go
+++ pkg/middlewares/trace/trace_logr.go
@@ -1,1 +1,24 @@
 package trace
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/go-jarvis/rum-gonic/pkg/logr"
+)
+
+func WithSpanLoggerMiddleware(c *gin.Context) {
+
+	span := TraceSpanFromContext(c)
+
+	if span.SpanContext().IsValid() {
+		log := logr.FromContext(c)
+
+		log = log.With(
+			"spanId", span.SpanContext().SpanID(),
+			"traceId", span.SpanContext().TraceID(),
+		)
+		// re-inject
+		_ = logr.WithContext(c, log)
+	}
+
+	c.Next()
+}
